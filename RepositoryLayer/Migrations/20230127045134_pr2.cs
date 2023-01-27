4fb9a50a51bd8abade194d14590da633e6adfca1
Hamza Fareed <hamza.fareed@pitb.gov.pk>
2023-01-27 11:16:36 +0500

    PR2 Database object updated

---
+++ RepositoryLayer/Migrations/20230127045134_pr2.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class pr2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SponsorCompanyNTN",
+                schema: "Core",
+                table: "PermitIssueApplication",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SponsorCompanyName",
+                schema: "Core",
+                table: "PermitIssueApplication",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SponsorPersonCNIC",
+                schema: "Core",
+                table: "PermitIssueApplication",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SponsorPersonName",
+                schema: "Core",
+                table: "PermitIssueApplication",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "SponsorTypeID",
+                schema: "Core",
+                table: "PermitIssueApplication",
+                type: "bigint",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SponsorCompanyNTN",
+                schema: "Core",
+                table: "PermitIssueApplication");
+
+            migrationBuilder.DropColumn(
+                name: "SponsorCompanyName",
+                schema: "Core",
+                table: "PermitIssueApplication");
+
+            migrationBuilder.DropColumn(
+                name: "SponsorPersonCNIC",
+                schema: "Core",
+                table: "PermitIssueApplication");
+
+            migrationBuilder.DropColumn(
+                name: "SponsorPersonName",
+                schema: "Core",
+                table: "PermitIssueApplication");
+
+            migrationBuilder.DropColumn(
+                name: "SponsorTypeID",
+                schema: "Core",
+                table: "PermitIssueApplication");
+        }
+    }
+}
